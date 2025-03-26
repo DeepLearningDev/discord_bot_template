d66351cfe24055ff7acf17e92ebcb4603a8dc5fd
K-eli-wtech <kewhite3@my.waketech.edu>
2025-03-26 19:39:17 -0400

    Added comments

--- botname/__main__.py
+++ botname/__main__.py
@@ -3,14 +3,17 @@ import crescent, hikari
 
 bot = hikari.GatewayBot(os.environ["TOKEN"], intents=hikari.Intents.ALL)
 client = crescent.Client(bot)
+# Location of the plugins folder for external bot commands
 client.plugins.load_folder("botname.plugins")
 
+# Example in command in main file
 @client.include
 @crescent.command(description="Ping Value")
 async def ping(ctx:crescent.Context) -> None:
   await ctx.respond("Pong")
 
 
+# Updates the commands
 if __name__ == "__main__":
   if os.name != "nt":
     import asynchio
